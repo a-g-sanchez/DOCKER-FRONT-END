d2a6f2bbc9ecde1a4e47548d0c2dd3cc9f32d4dc
a-g-sanchez <aarongsanchez@google.com>
2024-06-14 11:48:01 -0500

    Created demo frontend app structure to test with docker

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+const App = () => {
+
+  return (
+    <>
+     <main>
+      <h1>Hello World</h1>
+
+      <p>This is a docker test repo</p>
+     </main>
+    </>
+  )
+}
+
+export default App
